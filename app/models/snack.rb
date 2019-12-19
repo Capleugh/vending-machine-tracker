ce4e895cffd960f5c57b380693101f61bb5736e9
Carleigh Crockett <capleugh@gmail.com>
2019-12-19 10:31:29 -0700

    Adjust shoulda matchers from plural to singular form

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -2,4 +2,5 @@ class Snack < ApplicationRecord
   validates_presence_of :snack_type, :price
 
   has_many :machine_snacks
+  has_many :machines, through: :machine_snacks
 end
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Snack, type: :model do
 
   describe "relationships" do
     it { should have_many :machine_snacks}
+    it { should have_many(:machines).through(:machine_snacks)}
   end
 end
